638c8c9cd121a371953bf8b6d6013f33f952a8a4
Siphon880gh <weng.f.fung@gmail.com>
2023-06-17 15:52:10 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // useEffect that changes on a variable change a, b, or c
+  // var [a, setA] = useState("")
+  // var [b, setB] = useState("")
+  // var [c, setC] = useState("")
+
+  // useEffect(()=>{
+  //   setTimeout(()=>{
+  //     setName ([...nameArray, "N"]);
+  //     // name = "E D";
+  //   }, 2000);
+  // }, [a,b,c])
+
+  // useEffect that runs once
+  useEffect(()=>{
+
+    setTimeout(()=>{
+      setName ([...nameArray, "N"]);
+      // name = "E D";
+    }, 2000);
+
+  }, []) // runs once because [], similar to document ready
+
+  // useEffect would run whenever something rerenders?
+  // useEffect(()=>{
+  // })
+
+  // var [name, setName] = useState("Eric");
+  // useState returns an array
+  var useStateArray = useState(["E", "D"]);
+  var nameArray = useStateArray[0];
+  var setName = useStateArray[1];
+
+
+  // var [name] = useState("E");
+  // var name = "E"
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {nameArray.join(".")}
         </p>
         <a
           className="App-link"
